46f300aa3c1aa3d284a7b44f801b586edfccfaed
unknown <muhdqozeem@gmail.com>
2022-12-06 11:26:06 -0500

    created react element and displayed in browser

---
+++ my-react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <p>Hope I am welcomed?</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ skeleton1/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// create first element
+const myFirstElement = <h1>Hello, first React element</h1>
+
+// create root
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// render element
+root.render(myFirstElement);
